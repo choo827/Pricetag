46c427523a66353845dab231cbefefc985d67848
NoMoreViolence <ljh86029926@gmail.com>
2021-05-14 03:04:45 +0900

    feat: background setting

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,9 +1,11 @@
-// If your extension doesn't need a background script, just leave this file empty
+// 항시동작하는 스크립트
+// 백그라운드 작업 담당
+// 주기적으로 최신의 환전 금액 받아오기
+import { setCurrencyByBrowserLanguage } from '../utils/setCurrencyByBrowserLanguage';
 
-messageInBackground();
-
-// This needs to be an export due to typescript implementation limitation of needing '--isolatedModules' tsconfig
-export function messageInBackground() {
-  console.log('I can run your javascript like any other code in your project');
-  console.log('just do not forget, I cannot render anything !');
-}
+type Reason = 'install' | 'update' | 'chrome_update' | 'shared_module_update';
+chrome.runtime.onInstalled.addListener(details => {
+  const reason = details.reason as Reason;
+  if (reason !== 'install') return;
+  setCurrencyByBrowserLanguage();
+});
