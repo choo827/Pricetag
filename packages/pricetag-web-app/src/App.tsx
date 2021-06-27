19077ad46ace039da04aee0718db6ac11ae17bf0
NoMoreViolence <ljh86029926@gmail.com>
2021-06-27 21:05:02 +0900

    feat: change to monorepo

---
+++ packages/pricetag-web-app/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import './App.css';
+
+export const App: FC = props => (
+  <div className="App">
+    <div>PriceTag</div>
+    <button type="button" onClick={() => {}}>
+      Click me
+    </button>
+  </div>
+);
